43cd4029cf2e3929aa10066e1e54a9fb3782ed85
Avinash Subhash <avinashsubhash19@gmail.com>
2020-10-03 15:02:21 +0530

    Update time_format_converter.py

--- Python/time_format_converter.py
+++ Python/time_format_converter.py
@@ -1,17 +1,17 @@
 def time_conversion(s):
 
     hour=int(s[0]+s[1])
-    if s[8] == "P":
+    if s[9] == "P":
         if hour<12:
             hour+=12
         return(str(hour)+s[2:8])
-    elif s[8] == "A":
+    elif s[9] == "A":
         if hour == 12:
             hour=00
         return(str(0)+str(hour)+s[2:8])
 
 if __name__ =="__main__":
-    print("Enter the time: (HH:MM:SSPM or HH:MM:SSAM") #Here the last two characters of the string denotes AM or PM"
+    print("Enter the time: (HH:MM:SS PM or HH:MM:SS AM") #Here the last two characters of the string denotes AM or PM"
     s = input()
     result = time_conversion(s)
     print("Converted Time: ",result)
