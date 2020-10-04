1a08a25932880a4b64d9a0e80bec8da9b3f3ad43
Avinash Subhash <avinashsubhash19@gmail.com>
2020-10-04 16:12:35 +0530

    Covered all input constraints

--- Python/time_format_converter.py
+++ Python/time_format_converter.py
@@ -1,6 +1,5 @@
 def time_conversion(s):
-    assert s[10] == 'M' and (s[9] == 'A' or s[9] == 'P') , "Please enter in the valid format!"
-    assert s[2] == ':' and s[5] == ':' and s[8] == ' ', "Please enter in the valid format!"
+   
     hour=int(s[0]+s[1])
     if s[9] == "P":
         if hour<12:
@@ -10,13 +9,26 @@ def time_conversion(s):
         if hour == 12:
             hour=00
         return(str(0)+str(hour)+s[2:8])
-    
+flag = True   
+def validate(s):
+    global flag
+    if len(s) == 11:
+        if s[10] == 'M' and (s[9] == 'A' or s[9] == 'P'):
+            if s[2] == ':' and s[5] == ':' and s[8] == ' ':
+                if int(s[0:2]) >=0 and int(s[0:2]) <= 12:
+                    if int(s[3:5]) >=0 and int(s[3:5]) < 60:
+                        if int(s[6:8]) >= 0 and int(s[6:8]) < 60:
+                            return True
+    if flag == True:
+        flag = False
+    else:
+        print("Invalid Input.Enter the value again!")
+    return False
     
 if __name__ =="__main__":
     s = ""
     print("Enter the time: (HH:MM:SS PM or HH:MM:SS AM)")
-    while len(s) < 11:
-        s = input()
-        if len(s) < 11:
-            print("Invalid Input.Enter the value again!")
+    while not validate(s):
+        s = str(input())
+        
     print("Converted Time: ", time_conversion(s))
