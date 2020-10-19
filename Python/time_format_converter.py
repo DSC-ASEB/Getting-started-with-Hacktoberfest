2ccecbbc6e9ee96238675af1aece852c39bd090a
Avinash Subhash <avinashsubhash19@gmail.com>
2020-10-19 20:32:28 +0530

    Updated Codefactor errors

--- Python/time_format_converter.py
+++ Python/time_format_converter.py
@@ -1,5 +1,5 @@
 def time_conversion(s):
-    hour=int(s[0]+s[1])
+    hour=int(s[0])+int(s[1])
     if s[9] == "P":
         if hour<12:
             hour+=12
@@ -9,10 +9,10 @@ def time_conversion(s):
             hour=00
         return(str(0)+str(hour)+s[2:8])
 
-flag = True
+
    
 def validate(s):
-    global flag
+    
     if len(s) == 11:
         if s[10] == 'M' and (s[9] == 'A' or s[9] == 'P'):
             if s[2] == ':' and s[5] == ':' and s[8] == ' ':
@@ -20,15 +20,16 @@ def validate(s):
                     if int(s[3:5]) >=0 and int(s[3:5]) < 60:
                         if int(s[6:8]) >= 0 and int(s[6:8]) < 60:
                             return True
-    if flag :
-        flag = False
-    else:
-        print("Invalid Input. Please enter in correct format!")
     return False
     
 if __name__ =="__main__":
     s = ""
+    flag = False
     print("Enter the time: (HH:MM:SS PM or HH:MM:SS AM)")
     while not validate(s):
+        if flag:
+            print("Invalid Input. Please enter in correct format!")
+        else:
+            flag = True
         s = str(input())
     print("Converted Time:", time_conversion(s))
