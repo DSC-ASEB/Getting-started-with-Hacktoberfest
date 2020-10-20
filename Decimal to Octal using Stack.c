da8406a03366650dc031026122ee46e65396c18c
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-10-20 15:34:47 +0530

    Update Decimal to Octal using Stack.c

--- Decimal to Octal using Stack.c
+++ Decimal to Octal using Stack.c
@@ -1,81 +1,77 @@
-#include<stdio.h>
-#include<stdlib.h>
- #define SIZE 10
- #define TRUE 1
- #define FALSE 0
-/**********************************************/
- struct stack
- {
-     int TOP;
-     int Item [SIZE];
- };
-/**********************************************/
- struct stack S;
-/**********************************************/
- void Initalize(void)
- {
-     S.TOP=-1;
- }
- /**********************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+#define TRUE 1
+#define FALSE 0
+
+struct stack
+{
+  int TOP;
+  int Item[SIZE];
+};
+
+struct stack S;
+
+void Initalize(void)
+{
+  S.TOP = -1;
+}
+
 int Empty(void)
 {
-    if(S.TOP==-1)
-        return TRUE;
-        else
-        return FALSE;
+  if (S.TOP == -1)
+    return TRUE;
+  else
+    return FALSE;
 }
-/**********************************************/
-int Push( int x)
+
+int Push(int x)
 {
 
-    if(S.TOP== SIZE-1)
+  if (S.TOP == SIZE - 1)
     {
-        printf("stack overflow");
-    exit (1);
+      printf ("stack overflow");
+      exit (1);
     }
-    S.TOP=S.TOP+1;
-    S.Item[S.TOP]=x;
+  S.TOP = S.TOP + 1;
+  S.Item[S.TOP] = x;
 }
-/**********************************************/
- int Pop()
- {
-    int x;
-     if(Empty())
-    {
 
-     printf("stack underflow");
-     exit(1);
-    }
-     x=S.Item[S.TOP];
-     S.TOP=S.TOP-1;
-     return x;
- }
- /**********************************************/
- decimaltooct(int decimal)
- {
-      int r,x;
-     Initalize();
-     while(decimal!=0)
-     {
-         r=decimal%8;
-        decimal=decimal/8;
-         Push(r);
-
-     }
-     while(!Empty())
-     {
-         x=Pop();
-         printf("%d",x);
-     }
- }
-/**********************************************/
-main()
+int Pop()
 {
-    int x;
-    int decimal;
-    printf("enter the decimal no");
-    scanf("%d",&decimal);
-  decimaltooct(decimal);
+  int x;
+  if (Empty())
+    {
+      printf ("stack underflow");
+      exit (1);
+    }
+  x = S.Item[S.TOP];
+  S.TOP = S.TOP - 1;
+  return x;
 }
-/**********************************************/
 
+void decimalToOct(int decimal)
+{
+  int r, x;
+  Initalize ();
+  while (decimal != 0)
+    {
+      r = decimal % 8;
+      decimal = decimal / 8;
+      Push (r);
+
+    }
+  while (!Empty ())
+    {
+      x = Pop ();
+      printf ("%d", x);
+    }
+}
+
+void main ()
+{
+  int decimal;
+  printf ("Enter the decimal number: ");
+  scanf ("%d", &decimal);
+  decimalToOct(decimal);
+}
