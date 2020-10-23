6901192e89e422238b57643406e1834187397fcc
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-10-23 18:36:17 +0530

    Refactor

--- Java/DiamondPattern.java
+++ Java/DiamondPattern.java
@@ -1,36 +1,34 @@
 import java.util.Scanner;
 import static java.lang.System.exit;
 
-public class DiamondPattern {
-  public static void main(String[] args) {
-   int row ;
-   int i =0 , j = 0 ;
-   Scanner in = new Scanner(System.in);
-   for (;;) {
-    System.out.print("Enter rows for half of the diamond : ");     
-		   row = in.nextInt();
-		    if (row >= 1) {
-   for(i=0;i<=row;i++)
-   {
-     for(j=1;j<=row-i;j++)
-     System.out.print(" ");
-     for(j=1;j<=2*i-1;j++)
-       System.out.print("*");
-     System.out.print("\n");
-   }
- 
-   for(i=row-1;i>=1;i--)
-   {
-     for(j=1;j<=row-i;j++)
-     System.out.print(" ");
-     for(j=1;j<=2*i-1;j++)
-       System.out.print("*");
-     System.out.print("\n");
-   }
-            }else{
-         System.out.println("Wrong Input");
+public class Main {
+    public static void main(String[] args) {
+        int row;
+        int i = 0, j = 0;
+        Scanner in = new Scanner(System.in);
+        for (;;) {
+            System.out.print("Enter rows for half of the diamond : ");
+            row = in .nextInt();
+            if (row >= 1) {
+                for (i = 0; i <= row; i++) {
+                    for (j = 1; j <= row - i; j++)
+                        System.out.print(" ");
+                    for (j = 1; j <= 2 * i - 1; j++)
+                        System.out.print("*");
+                    System.out.print("\n");
+                }
+
+                for (i = row - 1; i >= 1; i--) {
+                    for (j = 1; j <= row - i; j++)
+                        System.out.print(" ");
+                    for (j = 1; j <= 2 * i - 1; j++)
+                        System.out.print("*");
+                    System.out.print("\n");
+                }
+            } else {
+                System.out.println("Wrong Input");
                 exit(1);
             }
-}
-}
+        }
+    }
 }
