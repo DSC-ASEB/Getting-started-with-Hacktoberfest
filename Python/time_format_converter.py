75476f31e1311403be11596bb0616398ae8b73e5
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-10-25 02:34:44 +0530

    Switch to regular expressions

--- Python/time_format_converter.py
+++ Python/time_format_converter.py
@@ -1,39 +1,26 @@
+#!/usr/bin/python
+import re
+
+
 def time_conversion(s):
-    hour=int(s[0]+s[1])
-    if s[9] == "P":
-        if hour<12:
-            hour+=12
-        return(str(hour)+s[2:8])
-    if s[9] == "A":
+    hour = int(s[00] + s[1])
+    if s[9] == 'P':
+        if hour < 12:
+            hour += 12
+        return str(hour) + s[2:8]
+    if s[9] == 'A':
         if hour == 12:
-            hour=00
-            return (str(0) + str(hour)+s[2:8])
+            hour = 00
+            return str(00) + str(hour) + s[2:8]
         if hour > 9 and hour < 12:
-            return(str(hour)+s[2:8])
-        if hour > 0 and hour < 10:
-            return (str(0) + str(hour)+s[2:8])
+            return str(hour) + s[2:8]
+        if hour > 00 and hour < 10:
+            return str(00) + str(hour) + s[2:8]
 
 
-   
-def validate(s):
-    
-    if len(s) == 11:
-        if s[10] == 'M' and (s[9] == 'A' or s[9] == 'P'):
-            if s[2] == ':' and s[5] == ':' and s[8] == ' ':
-                if int(s[0:2]) >0 and int(s[0:2]) <= 12:
-                    if int(s[3:5]) >=0 and int(s[3:5]) < 60:
-                        if int(s[6:8]) >= 0 and int(s[6:8]) < 60:
-                            return True
-    return False
-    
-if __name__ =="__main__":
-    s = ""
-    flag = False
-    print("Enter the time: (HH:MM:SS PM or HH:MM:SS AM)")
-    while not validate(s):
-        if flag:
-            print("Invalid Input. Please enter in correct format!")
-        else:
-            flag = True
-        s = str(input())
-    print("Converted Time:", time_conversion(s))
+if __name__ == '__main__':
+    s = ''
+    s = str(input('Enter the time (HH:MM:SS AM or HH:MM:SS PM): '))
+    while not re.match('^(1[0-2]|0[1-9]):[0-5][0-9]:[0-5][0-9] (AM|PM)$', s):
+        s = str(input('Invalid Input. Please enter in correct format!'))
+    print ('\n24 hour time is:', time_conversion(s))
